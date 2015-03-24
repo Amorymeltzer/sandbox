959953fe30e3bc61e032795821e91587206d1565
Amory Meltzer <Amorymeltzer@gmail.com>
2015-03-24 13:59:16 -0700

    I look here for ternary usage info so might as well actually have it

--- ternary.pl
+++ ternary.pl
@@ -8,9 +8,10 @@ use diagnostics;
 
 my $n = 2;
 
-if (@ARGV && $ARGV[0] =~ /^\d+$/)
-{
-    $n = $ARGV[0];
+if (@ARGV && $ARGV[0] =~ /^\d+$/) {
+  $n = $ARGV[0];
 }
 
+# Ternary ?: ($a = $test ? $b : $c;)
+# a is b if test, c if not
 printf "I have %d dog%s.\n", $n,($n == 1) ? "" : "s";
