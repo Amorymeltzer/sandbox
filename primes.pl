2e45a3edd46df5f43803d7a2bc8f9d82bf7dea94
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-09 13:55:33 -0700

    Formatting, deinterpolation

--- primes.pl
+++ primes.pl
@@ -3,41 +3,43 @@
 use strict;
 use warnings;
 
-my $truth = 0; # To evaluate primality and store first factor
+my $truth = 0;			# To evaluate primality and store first factor
 
 print "List or Number?\n";
-my $choice = <>; # User choice for a full list or number
-while ($choice !~ m/[ln]/io)
-{
-    print "Try again\n";
-    $choice = <>;
+my $choice = <>;		# User choice for a full list or number
+while ($choice !~ m/[ln]/io) {
+  print "Try again\n";
+  $choice = <>;
 }
 
-if ($choice =~ m/n/i) # Test one number
-{
-    print "Test what number? ";
+if ($choice =~ m/n/i)		# Test one number
+  {
+    print 'Test what number? ';
     my $number = <>;
     primeTest($number);
     print "First factor: $truth\n" if $truth != 0;
-    if ($truth == 0) {print "Prime!\n"} else {print "Not Prime :(\n"}
-}
-
-open(my $out, '>primes.txt');
-if ($choice =~ m/l/i) # The whole list up to that number
-{
-    print "Stop at what number? ";
-    my $number = <>;
-#    my $time = time;
-    foreach my $x (2..$number)
-    {
-	$truth = 0;
-	primeTest($x);
-#	print $out "$x\n" if $truth == 0;
-	print $out "$x\n" if $truth == 0;
+    if ($truth == 0) {
+      print "Prime!\n";
+    } else {
+      print "Not Prime :(\n";
     }
-#    $time = time - $time;
-#    print "$time seconds\n";
-}
+  }
+
+open my $out, '>', 'primes.txt';
+if ($choice =~ m/l/i)		# The whole list up to that number
+  {
+    print 'Stop at what number? ';
+    my $number = <>;
+    #    my $time = time;
+    foreach my $x (2..$number) {
+      $truth = 0;
+      primeTest($x);
+      #	print $out "$x\n" if $truth == 0;
+      print $out "$x\n" if $truth == 0;
+    }
+    #    $time = time - $time;
+    #    print "$time seconds\n";
+  }
 
 #########################################################
 # Subroutines
@@ -45,11 +47,10 @@ if ($choice =~ m/l/i) # The whole list up to that number
 
 # Test for primes
 sub primeTest
-{
+  {
     my $max = shift(@_);
-    foreach my $y (2..sqrt($max))
-    {
-	$truth = $max/$y if $max%$y == 0;
-	last if $max%$y == 0;
+    foreach my $y (2..sqrt($max)) {
+      $truth = $max/$y if $max%$y == 0;
+      last if $max%$y == 0;
     }
-}
+  }
