9fef16f69478334c516a9502d3eb727edf846353
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-12 09:37:28 -0700

    Tidy

--- election.pl
+++ election.pl
@@ -4,11 +4,12 @@ use strict;
 use warnings;
 
 my @numbers = (2..51);
-open (my $output, '>', "numbers.txt");
+open my $output, '>', 'numbers.txt' or die $!;
 
-print $output "=IF(C1=\"M\",B1,\"0\")";
+print $output '=IF(C1=\"M\",B1,\"0\")';
 
-foreach my $num (@numbers)
-{
-    print $output " + IF(C$num=\"M\",B$num,\"0\")";
+foreach my $num (@numbers) {
+  print $output " + IF(C$num=\"M\",B$num,\"0\")";
 }
+
+close $output or die $!;
