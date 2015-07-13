cd7a032dd11d9bb776c2a07eaf2e3a5831277df0
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-13 12:03:41 -0700

    Tidy up

--- flashcards.pl
+++ flashcards.pl
@@ -9,38 +9,36 @@ my $score = 0;
 my %wordHash;
 my @wordList;
 
-open (my $data, "<", "vocab.csv") || die $1;
-while (my $in = <$data>)
-{
-    chomp($in);
-    my @info = split (",",$in);
-    my $word = shift(@info);
-    my $def = substr($in,length($word)+1);
-    $def =~ s/"//gix;
-    $wordHash{$word} = $def;
-    @wordList = (@wordList,$word);
+open my $data, '<', 'vocab.csv' || die $1;
+while (my $in = <$data>) {
+  chomp $in;
+  my @info = split (",",$in);
+  my $word = shift @info;
+  my $def = substr $in, length($word)+1;
+  $def =~ s/"//gix;
+  $wordHash{$word} = $def;
+  @wordList = (@wordList,$word);
 }
-close ($data);
+close $data || die $1;
 
 my $length = keys %wordHash;
 
 my $infinite = -1;
-while ($infinite != 0)
-{
-    system("clear");
-    my $rand = int(rand($length-1))+1; # ignore the header
-    my $finally = $wordList[$rand];
-    print "Score: $score\n";
-    print color 'red';
-    print "$finally means\.\.\.\?";
-    my $name = <>;
-    print color 'cyan';
-    print "$wordHash{$finally}\n";
-    print color 'reset';
-    print "Next?";
-    $name = <>;
-    chomp($name);
-    $score++ if $name eq "=";
-    $score-- if $name eq "-";
-    exit if $name eq "exit";
+while ($infinite != 0) {
+  system("clear");
+  my $rand = int(rand($length-1))+1; # ignore the header
+  my $finally = $wordList[$rand];
+  print "Score: $score\n";
+  print color 'red';
+  print "$finally means\.\.\.\?";
+  my $name = <>;
+  print color 'cyan';
+  print "$wordHash{$finally}\n";
+  print color 'reset';
+  print 'Next?';
+  $name = <>;
+  chomp $name ;
+  $score++ if $name eq '=';
+  $score-- if $name eq '-';
+  exit if $name eq 'exit';
 }
