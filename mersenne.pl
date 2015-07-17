4e80953a576b3a7a85f88769986fc6359577e4c9
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-17 11:02:15 -0700

    Tidy up

--- mersenne.pl
+++ mersenne.pl
@@ -3,13 +3,13 @@
 use strict;
 use warnings;
 
-open (my $primes, '<primes.txt');
+open my $primes, '<', 'primes.txt' or die $!;
 
 my $count = 1;
-for (<$primes>)
-{
-    chomp;
-    my $value = 2**$_ - 1;
-    print "$count\t$_\t$value\n";
-    $count++;
+for (<$primes>) {
+  chomp;
+  my $value = 2**$_ - 1;
+  print "$count\t$_\t$value\n";
+  $count++;
 }
+close $primes or die $!;
