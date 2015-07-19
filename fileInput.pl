358b11abf45059990385919389aaf72254d14b80
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-19 13:48:34 -0700

    Tidy

--- fileInput.pl
+++ fileInput.pl
@@ -6,13 +6,12 @@ use warnings;
 my $file = 'input.csv';
 my ($line, $field1, $field2, $field3, $field4);
 
-open (F, $file) || die ("Could not open $file!");
+open F, $file || die "Could not open $file!";
 
-while ($line = <F>)
-{
-    ($field1,$field2,$field3,$field4) = split ',', $line;
-    print "$field1 : $field2 : $field3 : $field4";
+while ($line = <F>) {
+  ($field1,$field2,$field3,$field4) = split /,/, $line;
+  print "$field1 : $field2 : $field3 : $field4";
 }
 print "\n";
 
-close (F);
+close F;
