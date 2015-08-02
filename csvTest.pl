7d6fe57a1593c9a79effbf726b083dc70faa756e
Amory Meltzer <Amorymeltzer@gmail.com>
2015-08-02 11:31:35 -0700

    Tidy

--- csvTest.pl
+++ csvTest.pl
@@ -3,20 +3,20 @@
 use strict;
 use warnings;
 
-open (my $output, ">csv.csv") || die $1;
+open my $output, '>', 'csv.csv' || die $1;
 
-my $comma;
-my $value = "20,5";
-my $test = "\"20,5\"";
+my $value = '20,5';
+my $test = '"20,5"';
 
 print $output "$value,$test\n";
 fixComma($value);
 print $output "$value";
 
+close $output || die $!;
 
 sub fixComma
-{
+  {
     $_[0] =~ m/(.*),(.*)/i;
     $value = "\"$1,$2\"";
     return $value;
-}
+  }
