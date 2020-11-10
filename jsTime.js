b7257b16bcd0d0480acedbb444e6498147680b26
Amory Meltzer <Amorymeltzer@gmail.com>
2020-11-09 20:05:07 -0500

    jsTime.js: default to func.name if falsey name provided

--- jsTime.js
+++ jsTime.js
@@ -2,9 +2,12 @@
  * Do something like:
  * var timer = require('./path/to/jsTime.js).timer;
  */
-function timer(func, name=timer, count=10000) {
+function timer(func, name, count=10000) {
 	if (!func) {
 		throw new Error('No function provided');
+	}
+	if (!name) {
+		name = func.name;
 	}
 	console.time(name);
 	for (var i = 0; i < count; i++) {
