6c6f167cdc9e28aa3bd575e08277b6d6ded5f9bb
Amory Meltzer <Amorymeltzer@gmail.com>
2020-11-09 20:06:33 -0500

    jsTime.js: Invert count and name, easier to deal with this way with default name

--- jsTime.js
+++ jsTime.js
@@ -2,9 +2,9 @@
  * Do something like:
  * var timer = require('./path/to/jsTime.js).timer;
  */
-function timer(func, name, count=10000) {
+function timer(func, count=10000, name) {
 	if (!func) {
-		throw new Error('No function provided');
+		throw new Error('No function provided, call as timer(func, [count], [name])');
 	}
 	if (!name) {
 		name = func.name;
