05a691469d8f2b22be2154291fce2496990f6777
Amory Meltzer <Amorymeltzer@gmail.com>
2023-04-12 14:42:06 -0400

    birthday.pl: Add -p option to calculate number of people for a given percentage

--- birthday.pl
+++ birthday.pl
@@ -11,9 +11,20 @@ use 5.036;
 
 use Getopt::Long;
 my %config = (days=>365, number=>23);
-GetOptions(\%config, 'days|d=i', 'number|n=i', 'help' => \&usage);
+GetOptions(\%config, 'days|d=i', 'number|n=i', 'percent|p=i', 'help' => \&usage);
 
-printf "%.2f%%\n", birthday($config{days}, $config{number});
+use POSIX qw(ceil);
+
+if ($config{percent}) {
+  if ($config{percent} >= 100 || $config{percent} < 1) {
+    say 'Percent must be between 1 and 99, inclusive';
+    exit 1;
+  }
+  say people($config{days}, $config{percent});
+} else {
+  printf "%.2f%%\n", birthday($config{days}, $config{number});
+  # printf "%.2f%%\n", estimated($config{days}, $config{number});
+}
 
 ######## SUBROUTINES ########
 
@@ -21,12 +32,22 @@ printf "%.2f%%\n", birthday($config{days}, $config{number});
 sub birthday ($base, $sel) {
   return 100*(1-(1/$base**$sel)*factorial($base, $sel));
 }
+# How many eg people for a given probability?
+sub people ($base, $prob){
+  # Naive attempt at 42%->0.42, guaranteed by the i=integer aspect of Getopt::Long
+  $prob = $prob/100;
+  # I think this is right?  Probably some issues at high values of p
+  return ceil sqrt(2*$base*log(1/(1-$prob)));
+}
 
 sub factorial ($base, $sel) {
   use List::Util qw(reduce);
   return reduce { $a * $b } $base-$sel+1..$base;
 }
-
+# Simplified estimate, useful in theory for bigguns
+sub estimated ($base, $sel) {
+  return 100*(1-(exp -($sel**2)/(2*$base)))
+}
 
 #### Usage statement ####
 # Use POD or whatever?
@@ -34,8 +55,9 @@ sub factorial ($base, $sel) {
 # Final line must be unindented?
 sub usage {
   print <<"USAGE";
-Usage: $PROGRAM_NAME [-hnd]
+Usage: $PROGRAM_NAME [-hnpd]
       -n Specify the number of people in a room.  Default 23.
+      -p Specify a percent (1-99) for which to calculate the number of people required.
       -d Specify the number days from which to choose.  Default 365.
       -h Print this message
 USAGE
