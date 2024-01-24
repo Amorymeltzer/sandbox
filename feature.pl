e91dfbe8f3bc183fb062bbe3780870a63b0681e7
Amory Meltzer <Amorymeltzer@gmail.com>
2024-01-24 12:17:58 -0500

    feature: note postderef from v5.20
    https://perldoc.perl.org/perlref#Postfix-Dereference-Syntax

--- feature.pl
+++ feature.pl
@@ -7,7 +7,8 @@ use warnings;
 use feature qw(say);
 CORE::say 'Purple';
 
-# use 5.010; # Really 5.016 or 5.018 for proper working
+# use 5.010; # Really 5.016 or 5.018 for proper working, but given/when
+# deprecated in 5.038
 use feature 'switch';
 # No flag for turning off given/when warnings??  Lame.
 given (-1) {
@@ -21,7 +22,7 @@ given (-1) {
 # use feature 'fc';
 say CORE::fc 'aDddDAdSDjkA sdkja KAD';
 
-# use 5.020;
+# use 5.020; # Not experimental with 5.036
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
@@ -30,3 +31,9 @@ say addMe(3,4);
 sub addMe ($left, $right) {
     return $left + $right;
 }
+
+# use 5.020; # Not experimental with 5.024
+use feature 'postderef';
+
+my $asd = [1, 2];
+say $asd->@*;
