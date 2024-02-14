ba6a1a49c1474852fbe1274a71b67bb60306f544
Amory Meltzer <Amorymeltzer@gmail.com>
2024-02-14 09:46:48 -0500

    Tweak weirdOr reference

--- weirdOr.pl
+++ weirdOr.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
 # weirdOr.pl by Amory Meltzer 2012.11.08
 
+use 5.010;
 use strict;
 use warnings;
-use diagnostics;
 
 my $one = '1';
 my $two = '2';
@@ -14,8 +14,8 @@ my $onse;
 my $nun = $onse // $two; # $onse if defined, $two if not
 print "$nun\n";
 
-my $tre;
+my $tre = 0;
 
-$tre ||= 3; # original value if defined, new if not
+$tre ||= 3; # original value if true, new if not
 $tre++;
 print "$tre\n";
